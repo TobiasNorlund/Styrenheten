9c47dc61bd7378f64c91970520dff375b96942a2
Mikael Karlsson <karlsson.mkl@gmail.com>
2013-04-11 13:05:04 +0200

    Kod för uttagning av x samt theta

--- avbrott-test.c
+++ avbrott-test.c
@@ -15,30 +15,44 @@ void interrupt-beta()
         vänsterFram
         VänsterBak
         högerFram
-        högerBak
-		
-		Vid körning rakt fram:
-		xVänster = (vänsterBak+vänsterFram)*(1-theta*theta/2)/2;
-		xHöger = (högerBak+högerFram)*(1-theta*theta/2)/2;
-		
-		Om(xVänster < xHöger)
-		{
-			Reglera mot vänster vägg
-			
-			thetaVägg = (högerBak-högerFram)/avståndet mellan sensorerna	
-		}			
-		Annars
-		{
-			Reglera mot höger vägg
-			
-			thetaVägg = (vänsterFram-vänsterBak)/avståndet mellan sensorerna
-		}		
-		        
+        högerBak     
 
         omegaVägg = (thetaVägg[n]-thetaVägg [n-1])/T, T är tiden mellan sensorhämtningarna.
 		v = (x[n] - x[n-1])/T
 
     */
+		
+	// Aståndet mellan sensorerna i Y-led. Dvs mellan fronLeftSensor&backLeftSensor, samt frontRightSensor, backRightSensor.
+	int sensorDistance = 15;
 	
 	
+	// Av anledningen att x beror på theta kan det vara en idé att alltid uppdatera
+	// theta för
+	void setXLeft(int frontLeftSensor, int backLeftSensor, int theta)
+	{
+		x = (frontLeftSensor+backLeftSensor)*(2 - theta^2)/4
+		return;
+	}		
+	
+	void setXRight(int frontRightSensor, int backRightSensor, int theta)
+	{
+		x = (fronRightSensor + backRightSensor)*(2 - theta^2)/4;
+		return;
+	}		
+	
+	void setThetaLeft(int frontLeftSensor, int backLeftSensor)
+	{
+		theta = (frontLeftSensor - backLeftSensor)/sensorDistance;
+		return;
+	}
+	
+	void setThetaRight(int frontRightSensor, int backRightSensor)
+	{
+		theta = (backRightSensor - frontRightSensor)/sensorDistance;
+		return;
+	}
+	
+	// Hur gör vi för att lagra gamla värden vilket behövs för att ta reda på V samt OMEGA?
+	// Bör man skapa en array med två värden och hela tiden lagra över så vi får det nyaste
+	// att jobba med?
 }
