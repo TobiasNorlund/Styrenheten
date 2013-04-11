4ee1f77c31b6f6fc2c1d5be3f745266003243310
Mikael Karlsson <karlsson.mkl@gmail.com>
2013-04-11 14:18:50 +0200

    Bestamning av observator-varden ifran sensorvarden

--- avbrott-test.c
+++ avbrott-test.c
@@ -21,36 +21,67 @@ void interrupt-beta()
 		v = (x[n] - x[n-1])/T
 
     */
-		
-	// Aståndet mellan sensorerna i Y-led. Dvs mellan fronLeftSensor&backLeftSensor, samt frontRightSensor, backRightSensor.
-	int sensorDistance = 15;
+	
+	// ---------- Variabler ----------
+	// Aståndet mellan sensorerna i Y-led.
+	// Dvs mellan fronLeftSensor&backLeftSensor,
+	// samt frontRightSensor, backRightSensor.	
+	int sensorDistance = 15;	
+	
+	// Tiden mellan två timade-avbrott. Behövs för att kunna
+	// ta ut derivatorna v samt omega.
+	int t;''
+	int x;
+	int xOld;
+	int theta;
+	int thetaOld;
+	int v;
+	int omega;
 	
 	
+	// ----------- Funktioner --------------
 	// Av anledningen att x beror på theta kan det vara en idé att alltid uppdatera
 	// theta för
-	void setXLeft(int frontLeftSensor, int backLeftSensor, int theta)
+	void setXV(int frontSensor, int backSensor, int theta)
 	{
-		x = (frontLeftSensor+backLeftSensor)*(2 - theta^2)/4
+		xOld = x;
+		x = (frontSensor+backSensor)*(2 - theta^2)/4
+		v = (x - xOld)/t;
 		return;
-	}		
+	}			
 	
-	void setXRight(int frontRightSensor, int backRightSensor, int theta)
+	void setThetaOmegaLeft(int frontLeftSensor, int backLeftSensor)
 	{
-		x = (fronRightSensor + backRightSensor)*(2 - theta^2)/4;
-		return;
-	}		
-	
-	void setThetaLeft(int frontLeftSensor, int backLeftSensor)
-	{
+		thetaOld = theta;
 		theta = (frontLeftSensor - backLeftSensor)/sensorDistance;
+		omega = (theta - thetaOld)/t;
 		return;
 	}
 	
-	void setThetaRight(int frontRightSensor, int backRightSensor)
+	void setThetaOmegaRight(int frontRightSensor, int backRightSensor)
 	{
+		thetaOld = theta;
 		theta = (backRightSensor - frontRightSensor)/sensorDistance;
+		omega = (theta - thetaOld)/t;
 		return;
 	}
+	
+	void setObserver(int frontLeftSensor, int backLeftSensor, int frontRightSensor, int backRightSensor)
+	{
+		if((frontLeftSensor + backLeftSensor)/2 < (frontRightSensor + backRightSensor)/2))
+		{
+			setThetaOmegaLeft(frontLeftSensor, backLeftSensor);
+			setXV(frontLeftSensor, backLeftSensor);
+		}
+		else
+		{
+			setThetaOmegaRight(frontRightSensor, backRightSensor);
+			setXV(frontRightSensor,backRightSensor);
+		}
+		
+		return;
+	}
+	
 	
 	// Hur gör vi för att lagra gamla värden vilket behövs för att ta reda på V samt OMEGA?
 	// Bör man skapa en array med två värden och hela tiden lagra över så vi får det nyaste
