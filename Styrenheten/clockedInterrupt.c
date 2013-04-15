dbc8262c59eb348f06c4c79821b0ba5b1568fa86
David <mohlin.david@gmail.com>
2013-04-15 15:20:07 +0200

    interrupt

--- Styrenheten/clockedInterrupt.c
+++ Styrenheten/clockedInterrupt.c
@@ -6,16 +6,35 @@
  */ 
 
 #include <avr/interrupt.h>
-#include "spi_master.h"
+#include "../../TSEA27-include/message.h"
 #include "global.h"
-#include "commands.h"
+#include "../../TSEA27-include/SPI/spi_master.h"
 
-void timedInterrupt()
+void clockedInterrupt_init()
+{
+	mapDataToSendSize = 0;
+	debugMesssageBufferLength = 0;
+	//setup timers 1 och 3 16bit timers
+	//start clock and set clock devider.
+	TCCR1B=(1<<CS10)|(0<<CS11)|(1<<CS12);//clk/1024 (From prescaler)
+
+	//enable overflow interupt
+	TIMSK1=(1<<TOIE1);//overflow interupt
+	
+	TCNT1=0;//init value for counter 1
+}
+
+void updateState(uint8_t gyro1, uint8_t gyro2, uint8_t vRight, uint8_t vLeft)
+{
+	//TODO
+}
+
+ISR(TIMER1_OVF)
 {
 	//skicka request till sensorenehten att skicka data
 	uint8_t msgSend[32];
-	
-	SPI_MASTER_write(msgSend, SENSORDATA_REQUEST, 0);
+	SPI_set_sensor(START);
+	SPI_MASTER_write(msgSend, TYPE_REQUEST_SENSOR_DATA, 0);
 	
 	//pausa??
 	
@@ -24,47 +43,126 @@ void timedInterrupt()
 	uint8_t* type;
 	uint8_t* len;
 	SPI_MASTER_read(msgRecieve, type, len);
+	SPI_set_sensor(END);
 	//skicka vidare till PC
-	SPI_MASTER_write(msgRecieve, SEND_DEBUG_DATAPC, *len);
+	SPI_set_kom(START);
+	SPI_MASTER_write(msgRecieve, TYPE_DEBUG_DATA, *len);
+	SPI_MASTER_write(debugMesssageBuffer, TYPE_DEBUG_DATA, debugMesssageBufferLength);
+	debugMesssageBufferLength = 0;
 	//skriv in data
-	avstandsensor_1 = msgRecieve[0];
-	avstandsensor_2 = msgRecieve[1];
-	avstandsensor_3 = msgRecieve[2];
-	avstandsensor_4 = msgRecieve[3];
-	avstandsensor_5 = msgRecieve[4];
-	avstandsensor_6 = msgRecieve[5];
-	avstandsensor_7 = msgRecieve[6];
-	avstandsensor_8 = msgRecieve[7];
-	uint8_t gyro = msgRecieve[8];
-	//ska rotationssensorer in här?
+	uint8_t gyro1;
+	uint8_t gyro2;
+	uint8_t vRight;
+	uint8_t vLeft;
+	for(uint8_t i = 0; i < *len; ++i)
+	{
+		uint8_t id = msgRecieve[i]
+		switch(id)
+		{
+			case IDSENSOR1:
+				avstandsensor_1 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR2:
+				avstandsensor_2 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR3:
+				avstandsensor_3 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR4:
+				avstandsensor_4 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR5:
+				avstandsensor_5 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR6:
+				avstandsensor_6 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR7:
+				avstandsensor_7 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDSENSOR8:
+				avstandsensor_8 = msgRecieve[i+1];
+				++i;
+				break;
+			case IDGYROSENSOR:
+				gyro1 = msgRecieve[i+1];
+				gyro2 = msgRecieve[i+2];
+				i = i+2;
+				break;
+			case IDSPEEDRIGHT:
+				vRight = msgRecieve[i+1]
+				++i;
+				break;
+			case IDSPEEDLEFT:
+				vLeft = msgRecieve[i+1]
+				++i;
+				break;
+		}	
+	}
 	
 	//uppdatera tillstånd
-	//TODO
+	updateState(gyro1, gyro2, vRight, vLeft);
 	
 	//fråga om data från PC
-	SPI_MASTER_write(msgSend, MESSAGE_PC_REQUEST, 0);
-	
-	//pausa??
-	
-	SPI_MASTER_read(msgRecieve, type, len);
-	
-	
-	//nollstall timer
-}
-
-
-ISR(SPI_STC_vect)
-{
-	if( 1 ) //clockat interupt har kommit)
+	while(*type != TYPE_NO_PC_MESSAGES)
 	{
-		timedInterrupt();
-	}
-	else if(1)//annat..)
-	{
+		SPI_MASTER_write(msgSend, TYPE_REQUEST_PC_MESSAGE, 0); //flytta detta tills innan uppdatera tillstånd för att slippa paus
 		
-	}
-	else
-	{
+		//pausa??
 		
+		SPI_MASTER_read(msgRecieve, type, len);
+		
+		switch(*type)
+		{
+			case TYPE_MANUAL_COMMAND:
+			{
+				//lägg till msg[0] först i route
+				for(uint8_t i = routeLength; i > 0; --i)
+				{
+					route[routeLength] = route[routeLength-1];
+				}
+				route[0] = msgRecieve[0];
+				break;
+			}
+			case TYPE_CHANGE_PARM:
+			{
+				uint8_t ID = msgRecieve[0];
+				uint8_t val = msgRecieve[1];
+				
+				switch(ID)
+				{
+					case 1:
+					{
+						break; //TODO
+					}
+				}
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
 	}
+					
+	
+	//skicka all kartdata till komm
+	while(mapDataToSendSize != 0)
+	{
+		uint8_t x = mapDataToSend[mapDataToSendSize-2];
+		uint8_t y = mapDataToSend[mapDataToSendSize-1];
+		mapDataToSendSize = mapDataToSendSize-2;
+		msgSend[0] = x;
+		msgSend[1] = y;
+		msgSend[2] = map[y][x];
+		SPI_MASTER_write(msgSend, TYPE_MAP_DATA, 3); //flytta detta tills innan uppdatera tillstånd för att slippa paus	
+	}
+	SPI_set_kom(END);
 }
--- Styrenheten/global.h
+++ Styrenheten/global.h
@@ -14,12 +14,21 @@
 uint8_t mapX; //Xcoord för nuvarande ruta i kartindex
 uint8_t mapY; //Xcoord för nuvarande ruta i kartindex
 int8_t x; //offset från origo för x i nuvarande ruta mäts i 1/3 cm origo är i mitten av rutan
-int8_t; //offset från origo för x i nuvarande ruta mäts i 1/3 cm
+int8_t y; //offset från origo för x i nuvarande ruta mäts i 1/3 cm
 uint8_t v; //hastighet i 1/2 cm/s
 int8_t theta; //vridning i 360/255 grader
 int8_t omega; //vinkelhastighet i ?? grader/sekund
 
 uint8_t route[16];
+uint8_t routeLength;
+
+uint8_t debugMesssageBuffer[16];
+uint8_t debugMesssageBufferLength; //initieras i clockedInterupt_init
+
+uint8_t map[16][16];
+
+uint8_t mapDataToSend[16];
+uint8_t mapDataToSendSize; //initieras i clockedInterupt_init
 
 uint8_t avstandsensor_1;
 uint8_t avstandsensor_2;
