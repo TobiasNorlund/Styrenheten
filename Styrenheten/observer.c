e49281f2cecdf6c3a50272a4398c1acc5e455235
carka684 <carka684@student.liu.se>
2013-04-19 16:22:17 +0200

    observer

--- Styrenheten/observer.c
+++ Styrenheten/observer.c
@@ -1,53 +1,53 @@
 #include "global.h"
+#include "observer.h"
 #include "../../TSEA27-include/geometrics.h"
 #include <avr/io.h>
 
-void interrupt-beta()
+#define TIME 4 //i ms, ange i 2^n = t (TIME = 4 -> 16ms)
+
+
+
+void setXV(uint8_t shortForward, uint8_t shortRear)
 {
-	uint8_t time;
+	globals.xOld = globals.x;
+	globals.x = ((shortForward + shortRear)*(2 - globals.theta^2))>>2;
+	globals.v = (globals.x - globals.xOld)>>TIME;
+	return;
+}	
 	
-	void setXV(uint8_t shortForward, uint8_t shortRear)
-	{
-		xOld = x;
-		x = (shortForward + shortRear())*(2 - theta^2)/4
-		v = (x - xOld)/time;
-		return;
-	}	
-	
-	void setThetaOmegaLeft(uint8_t shortLeftForward, uint8_t shortLeftRear)
-	{
-		thetaOld = theta;
-		theta = (shortLeftForward - shortLeftRear)/SHORT_SENSOR_DISTANCE;
-		omega = (theta - thetaOld)/time;
-		return;
-	}
-	
-	void setThetaOmegaRight(uint8_t shortRightForward, uint8_t shortRightRear)
-	{
-		thetaOld = theta;
-		theta = (shortRightForward - shortRightRear)/SHORT_SENSOR_DISTANCE;
-		omega = (theta - thetaOld)/time;
-		return;
-	}
-	
-	void setObserver(int frontLeftSensor, int backLeftSensor, int frontRightSensor, int backRightSensor)
-	{
-		uint8_t shortLeftForward = getSensorShortLeftForward();
-		uint8_t shortLeftRear = getSensorShortLeftRear();
-		uint8_t shortRightForward = getSensorShortRightForward();
-		uint8_t shortRightRear = getSensorShortRightRear();
-		
-		if((shortLeftForward + shortLeftRear)/2 < (shortRightForward + shortRightRear)/2))
-		{
-			setThetaOmegaLeft(shortLeftForward, shortLeftRear);
-			setXV(shortLeftForward);
-		}
-		else
-		{
-			setThetaOmegaRight(shortRightForward, shortRightRear);
-			setXV(shortRightForward, shortRightRear);
-		}
-		return;
-	}
-	
+void setThetaOmegaLeft(uint8_t shortLeftForward, uint8_t shortLeftRear)
+{
+	globals.thetaOld = globals.theta;
+	globals.theta = (shortLeftForward - shortLeftRear)/SHORT_SENSOR_DISTANCE;
+	globals.omega = (globals.theta - globals.thetaOld)>>TIME;
+	return;
 }
+	
+void setThetaOmegaRight(uint8_t shortRightForward, uint8_t shortRightRear)
+{
+	globals.thetaOld = globals.theta;
+	globals.theta = (shortRightForward - shortRightRear)/SHORT_SENSOR_DISTANCE;
+	globals.omega = (globals.theta - globals.thetaOld)>>TIME;
+	return;
+}
+	
+void setObserver(int frontLeftSensor, int backLeftSensor, int frontRightSensor, int backRightSensor)
+{
+	uint8_t shortLeftForward = getSensorShortLeftForward();
+	uint8_t shortLeftRear = getSensorShortLeftRear();
+	uint8_t shortRightForward = getSensorShortRightForward();
+	uint8_t shortRightRear = getSensorShortRightRear();
+		
+	if((shortLeftForward + shortLeftRear)>>1 < (shortRightForward + shortRightRear)>>1)
+	{
+		setThetaOmegaLeft(shortLeftForward, shortLeftRear);
+		setXV(shortLeftForward,shortRightRear);
+	}
+	else
+	{
+		setThetaOmegaRight(shortRightForward, shortRightRear);
+		setXV(shortRightForward, shortRightRear);
+	}
+	return;
+}
+	
