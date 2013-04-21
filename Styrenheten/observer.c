eb0cd33dba54e87bf1dd8c35c2e50766d4f45954
mikka789 <karlsson.mkl@gmail.com>
2013-04-21 17:01:44 +0200

    Mest kommentering...

--- Styrenheten/observer.c
+++ Styrenheten/observer.c
@@ -10,8 +10,8 @@
 void setXV(uint8_t shortForward, uint8_t shortRear)
 {
 	globals.xOld = globals.x;
-	globals.x = ((shortForward + shortRear)*(2 - globals.theta^2))>>2;
-	globals.v = (globals.x - globals.xOld)>>TIME;
+	globals.x = ((shortForward + shortRear)*(2 - globals.theta^2))>>2; // >>2 Delar med 4
+	globals.v = (globals.x - globals.xOld)>>TIME; 
 	return;
 }	
 	
@@ -30,20 +30,36 @@ void setThetaOmegaRight(uint8_t shortRightForward, uint8_t shortRightRear)
 	globals.omega = (globals.theta - globals.thetaOld)>>TIME;
 	return;
 }
-	
+
+void setFromFront(uint8_t shortLeftForward, uint8_t shortRightForward)
+{
+	//TODO
+}
+
+
+/**
+ * Ska bli en helvetes massa if-satser beroende på
+ * vad man får för värden. Dvs reglera på olika sätt
+ * beroende på vilka sensordata vi får.
+ */
 void setObserver(int frontLeftSensor, int backLeftSensor, int frontRightSensor, int backRightSensor)
 {
 	uint8_t shortLeftForward = getSensorShortLeftForward();
 	uint8_t shortLeftRear = getSensorShortLeftRear();
 	uint8_t shortRightForward = getSensorShortRightForward();
 	uint8_t shortRightRear = getSensorShortRightRear();
+	
+	if((shortLeftRear == 255) & (shortRightRear == 255) & (shortLeftRear =/ 255) & shortRightForward =/ 255))
+	{
+		//TODO
 		
-	if((shortLeftForward + shortLeftRear)>>1 < (shortRightForward + shortRightRear)>>1)
+	}	
+	else if((shortLeftForward + shortLeftRear)>>1 < (shortRightForward + shortRightRear)>>1)
 	{
 		setThetaOmegaLeft(shortLeftForward, shortLeftRear);
 		setXV(shortLeftForward,shortRightRear);
 	}
-	else
+	else //Kan man skriva så?
 	{
 		setThetaOmegaRight(shortRightForward, shortRightRear);
 		setXV(shortRightForward, shortRightRear);
