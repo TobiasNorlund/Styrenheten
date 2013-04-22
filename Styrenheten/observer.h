49ee5df76c4ae144008fc2a8ce425bb74fc8bc69
carka684 <carka684@student.liu.se>
2013-04-22 12:39:44 +0200

    observer.h

---
+++ Styrenheten/observer.h
@@ -0,0 +1,20 @@
+﻿/*
+ * observer.h
+ *
+ * Created: 4/19/2013 3:45:46 PM
+ *  Author: carka684
+ */ 
+
+
+#ifndef OBSERVER_H_
+#define OBSERVER_H_
+
+void setXV(uint8_t shortForward, uint8_t shortRear);
+void setThetaOmegaLeft(uint8_t shortLeftForward, uint8_t shortLeftRear);
+void setThetaOmegaRight(uint8_t shortRightForward, uint8_t shortRightRear);
+void setObserver(int frontLeftSensor, int backLeftSensor, int frontRightSensor, int backRightSensor);
+
+
+
+
+#endif /* OBSERVER_H_ */
