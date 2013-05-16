906dfb6548997acba1f2e606bd3d4d94766c5876
David <mohlin.david@gmail.com>
2013-05-16 10:14:51 +0200

    flyttade kommentarer frn c till h

--- Styrenheten/pathfind.h
+++ Styrenheten/pathfind.h
@@ -26,15 +26,45 @@
 #include "global.h"
 #include "charting.h"
 
+/*
+*	uppdaterar metarutten samt skapar en rutt (dvs en serie kommandon) som tar roboten till nästa oupptäckta ruta
+*/
 void pathfind();
+/*
+*	hittar det element i metarutten som är nyligen upptäckt och är sist, samt flyttar alla element i metarutten som är före detta element och lägger till dessa i glob_adjecentNewSquares
+*/
 void truncateMetaRoute();
+/*
+*	tar bort alla element i glob_adjecentNewSquares som representerar en ruta som redan har blivit kartlagd
+*/
 void adjecentNewSquaresRemoveChartedSquares();
+/*
+*	skapar en metarutt om det inte finns en sådan.
+*/
 void initMetaRoute();
+/*
+*	lägger till alla element i glob_adjecentNewSquares till metarutten
+*/
 void createNewMetaRoute();
+/*
+*	returnerar avstånd från (x1, y1) till (x2, y2)
+*/
 uint16_t norm2pow2(int16_t fromX, int16_t fromY, int16_t toX, int16_t toY);
+/*
+*	skapar en rutt till första elementet i metarutten.
+*/
 void createRouteToNextMeta();
+/*
+*	returnerar kostnaden att gå in på rutan (toX, toY)
+*/
 uint8_t getCostMove(uint8_t toX, uint8_t toY);
+/*
+*	lägger till en x, y koordinat i en buffer om denna koordinat ej redan finns
+*/
 void addCurSQ(uint8_t* buffer, uint8_t* dirBuffer, uint8_t* bufferLength, uint8_t X, uint8_t Y, uint8_t dir);
+/*
+*	returnerar mininmum av a och b
+*/
 uint8_t min(uint8_t a, uint8_t b);
 
 #endif /* PATHFIND_H_ */
